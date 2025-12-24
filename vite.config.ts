e750cc3e6d73ca5b5d4634fd1784f983372b2c6b
Bob van Dongen <bob.van.dongen@outlook.com>
2025-12-24 10:28:24 +0100

    chore: change build output to docs/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/detectorapp-nl/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     sourcemap: true
   },
