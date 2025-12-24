ce41f811b0915a267a19f2037f7a6e04de92c183
Bob van Dongen <bob.van.dongen@outlook.com>
2025-12-24 10:24:41 +0100

    chore: include dist folder for GitHub Pages

---
+++ dist/assets/fossielenBelgieOL-WGVoM7he.js
@@ -0,0 +1,2 @@
+import{c as e}from"./fossielUtils-DesDB6xp.js";import"./index-CymeeoG1.js";import"./iconStyles-DMBxNC8p.js";async function c(){return e("BE","Fossielen België")}export{c as createFossielenBelgieLayerOL};
+//# sourceMappingURL=fossielenBelgieOL-WGVoM7he.js.map
---
+++ dist/assets/fossielenOL-CknsuBSu.js
@@ -0,0 +1,2 @@
+import{c as e}from"./fossielUtils-DesDB6xp.js";import"./index-CymeeoG1.js";import"./iconStyles-DMBxNC8p.js";async function n(){return e("NL","Fossielen Nederland")}export{n as createFossielenLayerOL};
+//# sourceMappingURL=fossielenOL-CknsuBSu.js.map
