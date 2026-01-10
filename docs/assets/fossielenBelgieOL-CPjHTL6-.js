e2c7445837c11c830cc2bc279f7a6b1df2070bcd
Bob van Dongen <bob.van.dongen@outlook.com>
2026-01-10 17:46:05 +0100

    v2.30.39

--- docs/assets/fossielenBelgieOL-CPjHTL6-.js
+++
@@ -1,2 +0,0 @@
-import{c as e}from"./fossielUtils-KXRkvgiy.js";import"./index-Z41AKCn6.js";import"./iconStyles-NsgY8PgQ.js";async function c(){return e("BE","Fossielen België")}export{c as createFossielenBelgieLayerOL};
-//# sourceMappingURL=fossielenBelgieOL-CPjHTL6-.js.map
---
+++ docs/assets/fossielenOL-DEMRPT16.js
@@ -0,0 +1,2 @@
+import{c as e}from"./fossielUtils-x4quo4e-.js";import"./index-BgTK8YnB.js";import"./iconStyles-oAP-zki5.js";async function n(){return e("NL","Fossielen Nederland")}export{n as createFossielenLayerOL};
+//# sourceMappingURL=fossielenOL-DEMRPT16.js.map
